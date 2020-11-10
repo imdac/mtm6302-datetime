237a2fb1396da6a4227531c18e952b1d44d1f358
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2020-11-10 10:46:38 -0500

    Adds starter files

---
+++ script.js
@@ -0,0 +1,18 @@
+const $date = document.getElementById('date')
+const $form = document.getElementById('form')
+const $datetime = document.getElementById('datetime')
+const $timer = document.getElementById('timer')
+const $start = document.getElementById('start')
+const $pause = document.getElementById('pause')
+
+$form.addEventListener('submit', function (e) {
+  e.preventDefault()
+})
+
+$start.addEventListener('click', function () {
+
+})
+
+$pause.addEventListener('click', function () {
+
+})
